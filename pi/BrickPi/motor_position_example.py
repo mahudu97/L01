2f695d4f6b58c2780941ec20b07be54511902e4c
Aswaath Balasubramanian <ab5616@eews305-055.ee.ic.ac.uk>
2018-10-18 18:26:36 +0100

    Back to manual testing

--- pi/BrickPi/motor_position_example.py
+++ pi/BrickPi/motor_position_example.py
@@ -17,72 +17,41 @@ motorParams.feedForwardGain = 255/20.0
 motorParams.minPWM = 18.0
 motorParams.pidParameters.minOutput = -255
 motorParams.pidParameters.maxOutput = 255
-# motorParams.pidParameters.k_p = 100
-# motorParams.pidParameters.k_i = 0.0
-# motorParams.pidParameters.k_d = 0.0
+motorParams.pidParameters.k_p = 500.0
+motorParams.pidParameters.k_i = 200.0
+motorParams.pidParameters.k_d = 100.0
 
-# interface.startLogging("AngleTuningLog100_0_0.txt")
-#
-# while True:
-#
-# 	angle = float(input("Enter an angle (rad): "))
-#
-# 	interface.increaseMotorAngleReferences(motors,[angle,-angle])
-#
-# 	while not interface.motorAngleReferencesReached(motors) :
-# 		motorAngles = interface.getMotorAngles(motors)
-# 		if motorAngles :
-# 			print "Motor angles: ", motorAngles[0][0], ", ", motorAngles[1][0]
-# 		time.sleep(0.1)
-#
-# 	print "Destination reached!"
-#
-# interface.stopLogging("AngleTuningLog100_0_0.txt")
+interface.setMotorAngleControllerParameters(motors[0],motorParams)
+interface.setMotorAngleControllerParameters(motors[1],motorParams)
 
-kp_test = [100,200,300,400,500,600,700,800,900,1000]
 
-ki_test = [0,100,200,300,400,500,600,700,800,900,1000]
+while True:
+	pCurr =float(input("Enter K_P: "))
+	iCurr =float(input("Enter K_I: "))
+	dCurr =float(input("Enter K_D: "))
 
-kd_test = [0,100,200,300,400,500,600,700,800,900]
-
-for pCurr in kp_test:
 	motorParams.pidParameters.k_p = pCurr
-	print "KP"
-	for iCurr in ki_test:
-		motorParams.pidParameters.k_i = iCurr
-		print "KI"
-		for dCurr in kd_test:
-			print "KD"
+	motorParams.pidParameters.k_i = iCurr
+	motorParams.pidParameters.k_d = dCurr
 
-			motorParams.pidParameters.k_d = dCurr
+	logFile= "ATL_" + str(pCurr)+"_" + str(iCurr)+ "_" +str(dCurr)+".csv"
+	interface.startLogging(logFile)
 
-			interface.setMotorAngleControllerParameters(motors[0],motorParams)
-			interface.setMotorAngleControllerParameters(motors[1],motorParams)
+	#angle = float(input("Enter an angle (rad): "))
+	angle = 6.2832 #2pi const
 
-			logFile = "AngleTuningLog"+str (pCurr) + "_" + str(iCurr) + "_" + str(dCurr) + ".txt"
+	interface.increaseMotorAngleReferences(motors,[angle,-angle])
 
+	while not interface.motorAngleReferencesReached(motors) :
+		motorAngles = interface.getMotorAngles(motors)
+		if motorAngles :
+			print "Motor angles: ", motorAngles[0][0], ", ", motorAngles[1][0]
+		time.sleep(0.1)
 
-			interface.startLogging(logFile)
+	print "Destination reached!"
 
-			angle = 6.2832 #2pi radians ie a circle
+interface.stopLogging(logFile)
 
-			interface.increaseMotorAngleReferences(motors,[angle,-angle])
-			count =0
-
-			while not interface.motorAngleReferencesReached(motors) :
-				count+=1
-				motorAngles = interface.getMotorAngles(motors)
-				if motorAngles :
-					print "Motor angles: ", motorAngles[0][0], ", ", motorAngles[1][0]
-				time.sleep(0.01)
-				if count>150:
-					break
-
-
-			print "Destination reached!"
-
-
-			interface.stopLogging()
 
 
 

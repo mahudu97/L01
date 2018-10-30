d30d42bb4f7e2eeec8eff47fd273a7e3b63a1122
Mayank <mayanksurana601@gmail.com>
2018-10-30 10:49:19 +0000

    Updated to test

--- pi/BrickPi/touch_sensors.py
+++ pi/BrickPi/touch_sensors.py
@@ -1,29 +1,44 @@
 import batmobile
 import time
 
+x = 'a'
 
-while not interface.motorAngleReferencesReached(motors) or (interface.getSensorValue(touch_port[0]) == 1) or (interface.getSensorValue(touch_port[1]) == 1):
+while (x != 'c'):
+
+	if(interface.getSensorValue(touch_port[1]) == 1) and (interface.getSensorValue(touch_port[0]) == 1) :
+		print ("both")
+		# batmobile.backward(20)
+		# time.sleep(2.5)
+		# ##Turn Right
+		# batmobile.turnRight(-1)		
+		# time.sleep(2.5)
+		continue
 	
 	# Bump on the right side
 	if(interface.getSensorValue(touch_port[0]) == 1):
-		
-		batmobile.backward(20)
-		time.sleep(2.5)
-		##turn Left
-		batmobile.turnLeft(-1)
-		time.sleepd(2.5)
+		print ("left")
+		# batmobile.backward(20)
+		# time.sleep(2.5)
+		# ##turn Left
+		# batmobile.turnLeft(-1)
+		# time.sleepd(2.5)
+		continue
 
 	# Bump on the left side
 	if(interface.getSensorValue(touch_port[1]) == 1):
 		###
-		batmobile.backward(20)
-		time.sleep(2.5)
-		##Turn Right
-		batmobile.turnRight(-1)		
-		time.sleep(2.5)
+		print ("right")
+		# batmobile.backward(20)
+		# time.sleep(2.5)
+		# ##Turn Right
+		# batmobile.turnRight(-1)		
+		# time.sleep(2.5)
+		continue
 	
-
-print "Destination reached!"
+	
+	
+	
+	x = input("Please tell me someone I should know, or enter 'quit': ")
 
 
 interface.terminate()

f864c57d7e7c3d4717b087b132176a3b6382a5e6
Aswaath Balasubramanian <ab5616@sprite15.doc.ic.ac.uk>
2018-11-03 11:48:30 +0000

    lab 3 added

--- pi/BrickPi/lab_three.py
+++ pi/BrickPi/lab_three.py
@@ -8,7 +8,7 @@ NUMBER_OF_PARTICLES = 100
 p_x = [0] * NUMBER_OF_PARTICLES # At the start every particle has its intial coords as 0,0,0
 p_y = [0] * NUMBER_OF_PARTICLES
 p_theta = [0] * NUMBER_OF_PARTICLES
-weight = [1/NUMBER_OF_PARTICLES] * NUMBER_OF_PARTICLES # In an actual example these would be updated
+weights = [1/NUMBER_OF_PARTICLES] * NUMBER_OF_PARTICLES # In an actual example these would be updated
 
 
 #the errors, need to be generated each time called
@@ -26,23 +26,28 @@ sigma_g = 0
 
 #updates arrays accordingly now
 #still confused about why this generates 100 points
-for i in range(0. 3):
+for i in range(0, 3):
     for j in range(0, 3):
         L01.forward(10)
         time.sleep(3.5)
+        particles = []
         for k in range(0, 99):
             error_e = random.gauss(mu,sigma_e)
             error_f = random.gauss(mu,sigma_f)
             p_x[k] += (10 + error_e) * math.cos(p_theta[k])
             p_y[k] += (10 + error_e) * math.sin(p_theta[k])
             p_theta[k] += error_f
-        print "drawParticles:" + str([p_x, p_y, p_theta])  # should print out the drawParticles
+            particles.append((p_x[k], p_y[k], p_theta[k]))
+        print "drawParticles:" + str(particles)  # should print out the drawParticles
     L01.left_90(1)                                         # did it in same way as testDraw
     time.sleep(2)
 
+    particles_angle = []
     for l in range (0,99):
         error_g = random.gauss(mu,sigma_g)
-        p_theta[k] += 90 + error_g
-    print "drawParticles:" + str([p_x, p_y, p_theta])
+        p_theta[l] += 90 + error_g
+        particles_angle.append((p_x[l], p_y[l], p_theta[l]))
+        
+    print "drawParticles:" + str(particles_angle)
 
 print "Square complete!"

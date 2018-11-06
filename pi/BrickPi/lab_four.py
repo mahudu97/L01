dab1fbbcf383015b296886ed823e5836b67192bc
Michael Lawrence <mkl16@point06.doc.ic.ac.uk>
2018-11-06 11:28:26 +0000

    created new file for experiment 4

---
+++ pi/BrickPi/lab_four.py
@@ -0,0 +1,129 @@
+import batmobile as L01
+import time
+import random
+import math
+import particleDataStructures as template
+
+NUMBER_OF_PARTICLES = 100
+p_x = [0.0] * NUMBER_OF_PARTICLES # At the start every particle has its intial coords as 0,0,0
+p_y = [0.0] * NUMBER_OF_PARTICLES
+p_theta = [0.0] * NUMBER_OF_PARTICLES
+weights = [1.0/NUMBER_OF_PARTICLES] * NUMBER_OF_PARTICLES # In an actual example these would be updated
+
+
+mu = 0 # update with actual value
+sigma_e = 0.037
+sigma_f = 0.037
+sigma_g = 0.021
+
+estimate_theta = 0.0
+estimate_x = 0.0
+estimate_y = 0.0
+
+
+#this is the function we need 3.1 of week 5 practical
+def calculate_likelihood(float x, float y, float theta, float z):
+
+
+def navigateToWaypoint( X, Y):
+    global estimate_x
+    global estimate_y
+    global estimate_theta
+    global p_x
+    global p_y
+    global p_theta
+    global weights
+    global mu
+    global sigma_e
+    global sigma_f
+    global sigma_g
+
+
+    print "Silly test " + str(estimate_x) +" "+ str(estimate_y) +" "+str(estimate_theta)
+    x_diff = X-estimate_x
+    y_diff = Y-estimate_y
+    dist = (x_diff**2 + y_diff**2)**0.5
+    angleDest = math.atan2(y_diff,x_diff) # returns an angle between - pi  and pi
+
+
+    angleRotate = angleDest - estimate_theta
+
+    L01.left_90(angleRotate/1.5708)
+    time.sleep(2.5)
+    #particles_rot = []
+    for k in range (100):
+        error_g = random.gauss(mu,sigma_g)
+        p_theta[k] += angleRotate + error_g
+    #   particles_rot.append((5*p_x[k], 5*p_y[k], p_theta[k]))
+    #print "drawParticles:" + str(particles_rot)  # should print out the drawParticles
+
+
+
+    L01.forward(dist)
+    time.sleep(dist*0.2)
+    #particles = []
+    for k in range(100):          #this code is only relevant for correction once a move has been logged
+        error_e = random.gauss(mu,sigma_e)
+        error_f = random.gauss(mu,sigma_f)
+        p_x[k] += (dist + error_e) * math.cos(angleDest)
+        p_y[k] += (dist + error_e) * math.sin(angleDest)
+        p_theta[k] += error_f
+    #    particles.append((5*p_x[k], 5*p_y[k], p_theta[k]))
+    #print "drawParticles:" + str(particles)  # should print out the drawParticles
+
+    # now to update current guess of position
+    estimate_x = 0.0
+    estimate_y = 0.0
+    estimate_theta = 0.0
+    for i in range(100):
+        estimate_x += p_x[i]*weights[i]
+        estimate_y += p_y[i]*weights[i]
+        estimate_theta += p_theta[i]*weights[i]
+    #print "Silly test 2 " + str(estimate_x) +" "+ str(estimate_y) +" "+str(estimate_theta)
+
+
+while True: #enter input co-ords, rotate, move, REPEAT
+    w_x = float(input("Enter X value of the waypoint: "))*100
+    w_y = float(input("Enter Y value of the waypoint: "))*100
+    navigateToWaypoint(w_x, w_y)
+
+
+
+
+#STUFF HERE IS FROM particleDataStructures DIRECTLY, NEEDS A LOT OF EDITTING
+#
+#
+#
+#
+#
+canvas = Canvas();	# global canvas we are going to draw on
+
+mymap = Map();
+# Definitions of walls
+# a: O to A
+# b: A to B
+# c: C to D
+# d: D to E
+# e: E to F
+# f: F to G
+# g: G to H
+# h: H to O
+mymap.add_wall((0,0,0,168));        # a
+mymap.add_wall((0,168,84,168));     # b
+mymap.add_wall((84,126,84,210));    # c
+mymap.add_wall((84,210,168,210));   # d
+mymap.add_wall((168,210,168,84));   # e
+mymap.add_wall((168,84,210,84));    # f
+mymap.add_wall((210,84,210,0));     # g
+mymap.add_wall((210,0,0,0));        # h
+mymap.draw();
+    return random.gauss(80,3) + 70*(math.sin(t)); # in cm
+
+particles = Particles();
+
+t = 0;
+while True:
+    particles.update();
+    particles.draw();
+    t += 0.05;
+    time.sleep(0.05);

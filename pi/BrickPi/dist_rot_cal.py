82652564872aae73d29be7f85bc1be1e57041fe4
Aswaath <aswaathb@gmail.com>
2018-11-13 15:26:19 +0000

    Fixes and calibrations

--- pi/BrickPi/dist_rot_cal.py
+++ pi/BrickPi/dist_rot_cal.py
@@ -4,17 +4,17 @@ import time
 L01 = batmobile
 
 
-L01.forward(40)
+L01.forward(100)
 time.sleep(5)
 
-L01.backward(40)
-time.sleep(5)
+#L01.backward(40)
+#time.sleep(5)
 
-L01.left_90(1)
-time.sleep(5)
+#L01.left_90(4)
+#time.sleep(5)
 
-L01.right_90(1)
-time.sleep(5)
+#L01.right_90(1)
+#time.sleep(5)
 
 
 

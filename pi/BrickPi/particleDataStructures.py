b2e757e2846283a5bc8991feb0f7c03e711202f7
Aswaath Balasubramanian <ab5616@point08.doc.ic.ac.uk>
2018-11-14 12:40:30 +0000

    added crosses for waypoints2

--- pi/BrickPi/particleDataStructures.py
+++ pi/BrickPi/particleDataStructures.py
@@ -81,4 +81,24 @@ def init_world(mymap):
     mymap.add_wall((168,84,210,84))    # f
     mymap.add_wall((210,84,210,0))     # g
     mymap.add_wall((210,0,0,0))        # h
+
+    ######adding crosses for waypoints#####
+    mymap.add_wall((79,30,89,30))       #h1
+    mymap.add_wall((84,25,84,35))       #v1
+    mymap.add_wall((175,30,185,30))     #h2
+    mymap.add_wall((180,25,180,35))     #v2
+    mymap.add_wall((175,54,185,54))     #h3
+    mymap.add_wall((180,49,180,59))     #v3
+    mymap.add_wall((133,54,143,54))     #h4
+    mymap.add_wall((138,49,138,59))     #v4
+    mymap.add_wall((133,168,143,168))   #h5
+    mymap.add_wall((138,163,138,173))   #v5
+    mymap.add_wall((109,168,119,168))   #h6
+    mymap.add_wall((114,163,114,173))   #v6
+    mymap.add_wall((109,84,119,84))     #h7
+    mymap.add_wall((114,79,114,89))     #v7
+    mymap.add_wall((79,84,89,84))       #h8
+    mymap.add_wall((84,79,84,89))       #v8
+    ########################################
+
     mymap.draw()

70e42e76f66b29e17eb1c9f46c7d9d80897a4807
Barrometer <pickup.edward@gmail.com>
2018-11-18 13:57:59 +0000

    Made function "make_histogram" to take a Location signature and return an array as Mehedi asked

---
+++ pi/BrickPi/lab 5.3.py
@@ -0,0 +1,10 @@
+import math
+
+
+def make_histogram(x): #take a LocationSignature in distance/angle space, and returns a LocationSignature in frequency/distance space
+	# distance will be discretised into chunks of 8cm
+	hist = [0]*32 # going to count how many occurences of each distance
+	for i in range len(x.sig):
+		value = math.floor(x.sig[i]/8);
+		hist[value]+=1
+	return hist
